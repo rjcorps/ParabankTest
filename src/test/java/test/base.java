2710be63026e53dbd80964686c43dc85de000bec
rjcorps <gdrivecore25@gmail.com>
2024-06-27 00:00:38 +0800

    first commit

---
+++ src/test/java/test/base.java
@@ -0,0 +1,22 @@
+package test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class base {
+
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver2 = new EdgeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		
+		
+	}
+}
